f01e71432fce24ec80efdb77c8e68a698d66d362
TheDummi <dummidevelopment@gmail.com>
2022-10-16 12:08:04 +0200

    e

--- Plugins/test/greeter.plugin.js
+++ Plugins/test/greeter.plugin.js
@@ -1,23 +1,31 @@
 /**
- * @name Test
+ * @name Greeter
  * @author Dummi
  * @authorId 482513687417061376
  * @version 0.0.1
- * @description idk yet
+ * @description Greets you.
  */
 
-module.exports = class Test {
+const settings = {
+    user: "Dummi"
+}
+
+module.exports = class Greeter {
     constructor(meta) {
         this.meta = meta;
     }
 
     load() {
         BdApi.showToast(`Loaded ${this.meta.name} ${this.meta.version}`)
+
+        this.start()
     }
     start() {
+        BdApi.showToast(`hello ${settings.user}!`)
 
+        this.stop()
     }
+
     stop() {
-        this.load()
     }
 }
